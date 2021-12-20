9349cb9e96ea1109eb6d5adb3a1872eadc0ae00c
Nimmo <dnimmo@gmail.com>
2021-12-20 13:53:50 +0000

    model updated to include selected image

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,66 @@
 module Main exposing (..)
 
 import Element exposing (..)
+import Element.Border as Border
 import Html exposing (Html)
 
 
-view : String -> Html msg
+type alias Model =
+    { photos : List { url : String }
+    , selected : String
+    }
+
+
+initialPhoto : String
+initialPhoto =
+    "1.jpeg"
+
+
+initialModel : Model
+initialModel =
+    { photos =
+        [ { url = initialPhoto }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selected = initialPhoto
+    }
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+viewThumbnail : Bool -> String -> Element msg
+viewThumbnail isSelected str =
+    image
+        [ Border.width <|
+            if isSelected then
+                2
+
+            else
+                1
+        ]
+        { src = urlPrefix ++ str
+        , description = ""
+        }
+
+
+view : Model -> Html msg
 view model =
     layout [] <|
         column
             []
             [ el [] <| text "Photo Groove"
             , row []
-                [ image []
-                    { src = "http://elm-in-action.com/1.jpeg"
-                    , description = ""
-                    }
+                [ row []
+                    (model.photos
+                        |> List.map .url
+                        |> List.map (\x -> viewThumbnail (x == model.selected) x)
+                    )
                 , image []
-                    { src = "http://elm-in-action.com/2.jpeg"
-                    , description = ""
-                    }
-                , image []
-                    { src = "http://elm-in-action.com/3.jpeg"
+                    { src = urlPrefix ++ "large/" ++ model.selected
                     , description = ""
                     }
                 ]
@@ -29,4 +69,4 @@ view model =
 
 main : Html msg
 main =
-    view "no model yet"
+    view initialModel
