633cc0df496cb54115a8eb1e5931c153871f1572
Nimmo <dnimmo@gmail.com>
2021-12-30 20:40:26 +0000

    use triple-quotes for JSON string

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -10,7 +10,7 @@ decoderTest : Test
 decoderTest =
     test "title defaults to (untitled)"
         (\_ ->
-            "{\"url\": \"fruits.com\", \"size\": 5}"
+            """{"url": "fruits.com",   "size": 5}"""
                 |> decodeString PhotoGroove.photoDecoder
                 |> Expect.equal
                     (Ok { url = "fruits.com", size = 5, title = "(untitled)" })
